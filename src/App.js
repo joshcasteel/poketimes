91cb3fbb3041259be80bf60abd4056c119f71972
joshcasteel <joshcasteelz@gmail.com>
2019-02-07 13:02:18 -0500

    initial route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={Home} />
+          <Route path="/" component={About} />
+          <Route path="/" component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
