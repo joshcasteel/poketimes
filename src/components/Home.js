e40a56a359a2482fc46286e307b8eca8298f3023
joshcasteel <joshcasteelz@gmail.com>
2019-02-08 12:12:16 -0500

    added jsonplaceholder api to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,39 @@
 import React from "react";
+import Axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga non quis
-        libero accusantium perspiciatis magnam, sapiente quidem velit, possimus
-        ab, quae odit dolorum. Esse beatae eum, qui odit sed iste!
-      </p>
-    </div>
-  );
-};
+class Home extends React.Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then(res =>
+      this.setState({
+        posts: res.data
+      })
+    );
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 export default Home;
